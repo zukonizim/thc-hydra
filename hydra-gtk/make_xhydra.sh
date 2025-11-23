157e3ca9a65c4d0cb07fe4eb8b11c91b2a477a39
zuko <zukonizim@gmail.com>
2025-11-23 13:02:04 +0300

    Add files via upload

---
+++ hydra-gtk/make_xhydra.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+echo "Trying to compile xhydra now (hydra gtk gui)"
+NOCONFIGURE=1 ./autogen.sh
+./configure
+make
+cp -v src/xhydra ..
+echo "The GTK GUI is ready, type \"./xhydra\" to start"
